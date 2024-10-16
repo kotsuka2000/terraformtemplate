3d7b99638836699742ce3388a435494597b65e4a
kotsuka2000 <86582283+kotsuka2000@users.noreply.github.com>
2024-10-16 13:38:14 +1100

    Delete data.tf

--- data.tf
+++
@@ -1,8 +0,0 @@
-data "external" "users" {
-  program = ["python3", "${path.module}/read_csv.py", "${path.module}/users.csv"]
-}
-
-locals {
-  users = jsondecode(data.external.users.result["users"])
-  team_names = distinct([for user in local.users : user.team_name])
-}
