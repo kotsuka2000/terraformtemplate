ff1aa393ce8940a84f0fdf1fa84bf9b0e95daadc
kotsuka2000 <86582283+kotsuka2000@users.noreply.github.com>
2024-10-16 13:39:12 +1100

    Delete users.tf

--- users.tf
+++
@@ -1,7 +0,0 @@
-resource "pagerduty_user" "user" {
-  for_each = { for user in local.users : "${user.first_name} ${user.last_name}" => user }
-
-  name  = "${each.value.first_name} ${each.value.last_name}"
-  email = each.value.email
-  role  = "limited_user"
-}
